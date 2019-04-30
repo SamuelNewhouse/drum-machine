53e621757e52b7ae0c3180e96f3127711f5b12f4
unknown <samuel_newhouse@yahoo.com>
2019-04-30 14:50:23 -0500

    Map pad data to <DrumPad> components

--- src/Components/DrumPads.js
+++ src/Components/DrumPads.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import DrumPad from './DrumPad';
+import pads from '../data/pads';
 
 const DrumPads = () => {
-  return <div></div>
-}
+  return (
+    <div id="drum-pads">
+      {Array.from(pads, ([key, value]) =>
+        <DrumPad id={key} key={key} letter={value.letter} url={value.url} />
+      )}
+    </div>
+    )
+  }
 
 export default DrumPads;

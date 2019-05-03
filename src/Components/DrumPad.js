5f57d56fa5da0dbbf67163086326827e59f1f478
unknown <samuel_newhouse@yahoo.com>
2019-05-03 12:39:11 -0500

    Make <DrumPad> completely update through props

--- src/Components/DrumPad.js
+++ src/Components/DrumPad.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import playPad from '../util/playPad';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { playPad } from '../actions'
 
-const DrumPad = ({ letter, id, url }) => {
-  return (
-    <button type="button" id={id} className="drum-pad" onMouseDown={() => { playPad(id, letter) }}>
-      {letter}
-      <audio id={letter} src={url} ></audio>
-    </button>
-  )
+class DrumPad extends Component {
+  constructor(props) {
+    super(props);
+    this.buttonRef = React.createRef();
+    this.audioRef = React.createRef();
+  }
+
+  componentDidUpdate({pressed}) {
+    if(pressed) {
+      this.buttonRef.current.focus();
+      this.audioRef.current.currentTime = 0;
+      this.audioRef.current.play();
+    }
+  }
+
+  render() {
+    const { letter, id, url, pressed, play } = this.props;
+
+    return (
+      <button
+        type="button"
+        id={id}
+        className={"drum-pad " + pressed}
+        onMouseDown={() => { play(letter) }}
+        ref={this.buttonRef}
+      >
+        {letter}
+        <audio id={letter} src={url} ref={this.audioRef} ></audio>
+      </button>
+    )
+  }
 }
 
-export default DrumPad;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    pressed: state[ownProps.letter]
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    play: letter => dispatch(playPad(letter))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DrumPad);

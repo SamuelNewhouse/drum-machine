31b236569a7e1853bf8e42babf1d72a84fc86a49
unknown <samuel_newhouse@yahoo.com>
2019-05-03 14:12:32 -0500

    Pass text prop into <Display>

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import pads from '../data/pads';
 
-const Display = text => {
-  return <div id="display"></div>
+const Display = ({text}) => {
+  return <div id="display">{text}</div>
 }
 
-export default Display;
+const mapStateToProps = state => {
+  if (!state.lastDrumPad)
+    return { text: null };
+
+  return { text: pads.get(state.lastDrumPad).id };
+}
+export default connect(mapStateToProps, null)(Display);

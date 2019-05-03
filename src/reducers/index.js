d7f00ca7e946447e731772e20ec919210eebc9b2
unknown <samuel_newhouse@yahoo.com>
2019-05-03 12:36:42 -0500

    Begin making reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,33 @@
+import {
+  START_PAD,
+  END_PAD,
+  RECORD_PAD,
+  SET_RECORDING_STATE,
+  SET_RECORDING_POSITION,
+  SET_RECORDING,
+  RecordingStates,
+} from '../actions';
+
+import pads from '../data/pads'
+
+const initialState = {
+  recordingState: RecordingStates.PAUSED,
+  lastDrumPad: null,
+  lastDrumPadTime: null
+};
+for (let key of pads.keys())
+  initialState[key] = "";
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case START_PAD:
+      return Object.assign({}, state, { [action.letter]: 'pressed' })
+    case END_PAD:
+      return Object.assign({}, state, { [action.letter]: '' })
+
+    default:
+      return state;
+  }
+}
+
+export default reducer;

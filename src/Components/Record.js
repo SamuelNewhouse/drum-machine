18f11d99da2f236fad20b6992f6dcfb8e44c2f25
unknown <samuel_newhouse@yahoo.com>
2019-05-06 17:18:13 -0500

    Add top bar buttons to <Record>

--- src/Components/Record.js
+++ src/Components/Record.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
+import first from '../svg/first.svg';
+import play from '../svg/play3.svg';
+import pause from '../svg/pause2.svg';
+import record from '../svg/record.svg';
+import last from '../svg/last.svg';
 const Record = () => {
-  return <div id="record">Record</div>
+  return (
+    <div id="record">
+      <div id="record-top-bar">
+        <button type="button"><img alt="First" src={first}></img></button>
+        <button type="button"><img alt="Play" src={play}></img></button>
+        <button type="button"><img alt="Pause" src={pause}></img></button>
+        <button type="button"><img alt="Record" src={record}></img></button>
+        <button type="button"><img alt="Last" src={last}></img></button>
+      </div>
+      {/* Copy, Paste, Clear */}
+    </div>
+  )
 }
 export default Record;

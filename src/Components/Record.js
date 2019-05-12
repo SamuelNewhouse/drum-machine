30379715e80f03e34590dd87caae92d130889eb2
unknown <samuel_newhouse@yahoo.com>
2019-05-11 22:04:57 -0500

    Add goto start and goto end to <Record>

--- src/Components/Record.js
+++ src/Components/Record.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { PAUSED, RECORDING, setRecordingState, beginPlayRecording } from '../actions';
+import { PAUSED, RECORDING, setRecordingState, beginPlayRecording, setRecordingPosition } from '../actions';
 
 import first from '../svg/first.svg';
 import play from '../svg/play3.svg';
@@ -25,20 +25,20 @@ class Record extends Component {
   render() {
     const {
       recordingData,
-      recordingState,
       position,
       playRecording,
       pauseRecording,
-      startRecording } = this.props;
+      startRecording,
+      setRecordingPosition } = this.props;
 
     return (
       <>
         <div id="recording-top-bar">
-          <button type="button"><img alt="First" src={first}></img></button>
+          <button type="button" onMouseDown={() => setRecordingPosition(0)}><img alt="First" src={first}></img></button>
           <button type="button" onMouseDown={playRecording}><img alt="Play" src={play}></img></button>
           <button type="button" onMouseDown={pauseRecording}><img alt="Pause" src={pause}></img></button>
           <button type="button" onMouseDown={startRecording}><img alt="Record" src={record}></img></button>
-          <button type="button"><img alt="Last" src={last}></img></button>
+          <button type="button" onMouseDown={() => setRecordingPosition(recordingData.length - 1)}><img alt="Last" src={last}></img></button>
         </div>
         <div id="recording" ref={this.recordingDivRef}>
           <div>
@@ -67,7 +67,6 @@ class Record extends Component {
 const mapStateToProps = state => {
   return {
     recordingData: state.recordingData,
-    recordingState: state.recordingState,
     position: state.position
   }
 }
@@ -77,6 +76,7 @@ const mapDispatchToProps = dispatch => {
     playRecording: () => dispatch(beginPlayRecording()),
     pauseRecording: () => dispatch(setRecordingState(PAUSED)),
     startRecording: () => dispatch(setRecordingState(RECORDING)),
+    setRecordingPosition: position => dispatch(setRecordingPosition(position))
   }
 }
 

2e35f662ae72f59a37c56d2ed3f6a19d8ac76f25
unknown <samuel_newhouse@yahoo.com>
2019-05-22 15:51:58 -0500

    Add Edit placeholder to <RecordEntry>

--- src/Components/RecordEntry.js
+++ src/Components/RecordEntry.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const RecordEntry = ({ className, name, delay, onMouseDown }) => {
-  return <div className={className} onMouseDown={onMouseDown}>{name} - {delay}</div>
+  return (
+    <div className={className} onMouseDown={onMouseDown}>
+      <span className="info">{name} - {delay}</span>
+      <button type="button" className="edit">Edit</button>
+    </div>
+  )
 }
 
 export default RecordEntry;

6ab17bb33322a4a3a1cd8ea8cde8bda8a3ea11b2
unknown <samuel_newhouse@yahoo.com>
2019-05-27 15:23:31 -0500

    Add audio and more styles to <EditForm>

--- src/Components/EditForm.js
+++ src/Components/EditForm.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import pads from '../data/pads';
-import { editEntry } from '../actions';
+import { editEntry, playPad } from '../actions';
 import { setRecordingState, PAUSED } from '../actions';
 
 class EditForm extends Component {
@@ -15,18 +15,22 @@ class EditForm extends Component {
   }
 
   render() {
-    const { position, recordingData, exitEditMode } = this.props;
+    const { position, recordingData, exitEditMode, playPad } = this.props;
     const { letter, delay } = recordingData[position];
     return (
       <form className="edit-form" onSubmit={this.onSubmit}>
-        <label htmlFor="instrument">Instrument</label>
-        <select className="custom-select" id="instrument" defaultValue={letter}>
-          {Array.from(pads, ([key, v]) =>
-            <option key={key} value={key}>{key + " - " + v.id}</option>)
-          }
-        </select>
-        <label htmlFor="delay">Delay</label>
-        <input type="number" id="delay" min="0" defaultValue={delay}></input>
+        <div className="form-group">
+          <label htmlFor="instrument">Instrument</label>
+          <select className="custom-select" id="instrument" defaultValue={letter} onChange={event => playPad(event.target.value)}>
+            {Array.from(pads, ([key, v]) =>
+              <option key={key} value={key}>{key + " - " + v.id}</option>)
+            }
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="delay">Delay</label>
+          <input type="number" className="form-control" id="delay" min="0" max="3600000" defaultValue={delay}></input>
+        </div>
         <input type="submit" value="Save"></input>
         <button type="button" onClick={exitEditMode} >Cancel</button>
       </form>
@@ -41,8 +45,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     editEntry: (position, letter, delay) => dispatch(editEntry(position, letter, delay)),
-    exitEditMode: () => dispatch(setRecordingState(PAUSED))
-
+    exitEditMode: () => dispatch(setRecordingState(PAUSED)),
+    playPad: letter => dispatch(playPad(letter))
   }
 }
 

80236fe44f183d657542dbc5a94eb139067d714a
unknown <samuel_newhouse@yahoo.com>
2019-05-27 10:38:12 -0500

    Add more functionality to <EditForm>

--- src/Components/EditForm.js
+++ src/Components/EditForm.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import pads from '../data/pads';
+import { editEntry } from '../actions';
+import { setRecordingState, PAUSED } from '../actions';
 
-const EditForm = (position) => {
-  return (
-    <form className="edit-form">
-      <label htmlFor="instrument">Instrument</label>
-      <select className="custom-select" id="instrument">
-        {Array.from(pads, ([key, value]) =>
-          <option key={key} value={value.id}>{key + " - " + value.id}</option>)
-        }
-      </select>
-      <label htmlFor="delay">Delay</label>
-      <input type="number" id="delay" min="0"></input>
-    </form>
-  )
+class EditForm extends Component {
+  onSubmit = event => {
+    event.preventDefault();
+    const target = event.target;
+    const { position, editEntry, exitEditMode } = this.props;
+
+    editEntry(position, target.instrument.value, Number(target.delay.value));
+    exitEditMode();
+  }
+
+  render() {
+    const { position, recordingData, exitEditMode } = this.props;
+    const { letter, delay } = recordingData[position];
+    return (
+      <form className="edit-form" onSubmit={this.onSubmit}>
+        <label htmlFor="instrument">Instrument</label>
+        <select className="custom-select" id="instrument" defaultValue={letter}>
+          {Array.from(pads, ([key, v]) =>
+            <option key={key} value={key}>{key + " - " + v.id}</option>)
+          }
+        </select>
+        <label htmlFor="delay">Delay</label>
+        <input type="number" id="delay" min="0" defaultValue={delay}></input>
+        <input type="submit" value="Save"></input>
+        <button type="button" onClick={exitEditMode} >Cancel</button>
+      </form>
+    )
+  }
 }
 
-export default EditForm;
+const mapStateToProps = state => {
+  return { recordingData: state.recordingData }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    editEntry: (position, letter, delay) => dispatch(editEntry(position, letter, delay)),
+    exitEditMode: () => dispatch(setRecordingState(PAUSED))
+
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditForm);

0fddfeae6d5790d81849be81072bb5b67dd6c432
unknown <samuel_newhouse@yahoo.com>
2019-05-28 09:02:21 -0500

    Created volume slider

--- src/Components/Volume.js
+++ src/Components/Volume.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Volume = () => {
-  return <div id="volume">Volume</div>
+  return (
+  <div id="volume">
+    <label htmlFor="volumeSlider">Volume</label>
+    <input id="volumeSlider" type="range" orient="vertical" min="0" max="100"></input>
+  </div>
+  )
 }
 
 export default Volume;

ca3989c907b9816f78359c42eb9f0612ee55c48b
unknown <samuel_newhouse@yahoo.com>
2019-05-28 09:24:12 -0500

    Add volume slider svgs

--- src/Components/Volume.js
+++ src/Components/Volume.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import volumeIncrease from '../svg/volume-increase.svg';
+import volumeDecrease from '../svg/volume-decrease.svg';
+
 const Volume = () => {
   return (
   <div id="volume">
-    <label htmlFor="volumeSlider">Volume</label>
-    <input id="volumeSlider" type="range" orient="vertical" min="0" max="100"></input>
+    <img src={volumeIncrease} alt="Increase Volume"></img>
+    <input type="range" orient="vertical" min="0" max="100"></input>
+    <img src={volumeDecrease} alt="Decrease Volume"></img>
   </div>
   )
 }

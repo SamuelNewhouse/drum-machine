a9eb2a4c69f8e8604ebf73f0b3d7ac8f53a51146
unknown <samuel_newhouse@yahoo.com>
2019-05-28 09:55:59 -0500

    Add full volume control functionality to <Volume>

--- src/Components/Volume.js
+++ src/Components/Volume.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { setVolume } from '../actions';
 
 import volumeIncrease from '../svg/volume-increase.svg';
 import volumeDecrease from '../svg/volume-decrease.svg';
 
-const Volume = () => {
+const Volume = ({ volume, setVolume }) => {
   return (
-  <div id="volume">
-    <img src={volumeIncrease} alt="Increase Volume"></img>
-    <input type="range" orient="vertical" min="0" max="100"></input>
-    <img src={volumeDecrease} alt="Decrease Volume"></img>
-  </div>
+    <div id="volume">
+      <img src={volumeIncrease} alt="Increase Volume" />
+      <input type="range" orient="vertical" min="0" max="100"
+        value={volume}
+        onChange={event => setVolume(event.target.value)}
+      />
+      <img src={volumeDecrease} alt="Decrease Volume" />
+    </div>
   )
 }
 
-export default Volume;
+const mapStateToProps = state => {
+  return {
+    volume: state.volume
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setVolume: volume => dispatch(setVolume(volume))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Volume);

3d785c660163d57de14ad309125294c5ac15e863
unknown <samuel_newhouse@yahoo.com>
2019-05-30 11:57:00 -0500

    Create <AddEntry>

---
+++ src/Components/AddEntry.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import EntryForm from './EntryForm';
+import { setRecordingState, EDITING } from '../actions';
+
+const AddEntry = ({ classes, name, delay, isBeingEdited, onMouseDown, startEdit }) => {
+  return (
+    <div className={classes} onMouseDown={onMouseDown}>
+      <span className="info">{name} - {delay}</span>
+      <button type="button" className="edit" onClick={startEdit}>Add</button>
+      {isBeingEdited && <EntryForm/>}
+    </div>
+  )
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    startEdit: () => dispatch(setRecordingState(EDITING))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(AddEntry);

6087ae3f780297e5682bf1c52be904902a569a6b
unknown <samuel_newhouse@yahoo.com>
2019-06-22 16:44:24 -0500

    Build commit

---
+++ precache-manifest.3f66d579208c94bc4f37730432c93405.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a6b3f081beb461198b3726c8a7402a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f794c5c686a631eb7783",
+    "url": "./static/css/2.97b89eb4.chunk.css"
+  },
+  {
+    "revision": "a539c68e53d71f8b04f0",
+    "url": "./static/css/main.ea9fe0e1.chunk.css"
+  },
+  {
+    "revision": "f794c5c686a631eb7783",
+    "url": "./static/js/2.1568ee1c.chunk.js"
+  },
+  {
+    "revision": "a539c68e53d71f8b04f0",
+    "url": "./static/js/main.4d0ecc38.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "ab7f548ce901522d563d223f618ceace",
+    "url": "./static/media/bin.ab7f548c.svg"
+  },
+  {
+    "revision": "80d3557e23d3fb3613cea9c1da75bfbd",
+    "url": "./static/media/copy.80d3557e.svg"
+  },
+  {
+    "revision": "2c5692053fd75415fb110bb7f3377e7f",
+    "url": "./static/media/first.2c569205.svg"
+  },
+  {
+    "revision": "a47bad7f620384d7331ad89b37103757",
+    "url": "./static/media/last.a47bad7f.svg"
+  },
+  {
+    "revision": "c81c13c58b33e0bcbe56ef9aab769ff3",
+    "url": "./static/media/pause2.c81c13c5.svg"
+  },
+  {
+    "revision": "b341547744575f55e623d813939a1a52",
+    "url": "./static/media/play3.b3415477.svg"
+  },
+  {
+    "revision": "4e024e149c96e7051599283a82df6fc2",
+    "url": "./static/media/record.4e024e14.svg"
+  },
+  {
+    "revision": "5c9e5972d1c9fc8aaf7d4f7e5983ee04",
+    "url": "./static/media/volume-decrease.5c9e5972.svg"
+  },
+  {
+    "revision": "6d539866305bdff75b548f64e2209cc1",
+    "url": "./static/media/volume-increase.6d539866.svg"
+  }
+]);
